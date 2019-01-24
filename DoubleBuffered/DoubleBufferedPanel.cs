702ec64b8e363d328403e8fcc065f715f07d4663
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2019-01-24 12:45:56 -0600

    Initial Commit

---
+++ DoubleBuffered/DoubleBufferedPanel.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace DoubleBuffered
+{
+    public class DoubleBufferedPanel : Panel
+    {
+        public DoubleBufferedPanel()
+        {
+            this.DoubleBuffered = true;
+        }
+    }
+}
